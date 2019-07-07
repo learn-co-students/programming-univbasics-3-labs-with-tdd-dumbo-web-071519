cf8d6e70c36af7ed615ab61703ad2cd5f08c2807
Alice Ly Ms <alicehly@gmail.com>
2019-07-07 02:26:06 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -2,7 +2,11 @@
 first_number = 1
 
 second_number = 2
+
 sum = first_number + second_number
-difference = firstnumber - second_number
+
+difference = first_number - second_number
+
 product = first_number * second_number
+
 quotient = first_number / second_number
